41adadabbd3f9f2562cc0a0ff31741ae551de6d0
bek <bekrye@gmail.com>
2023-07-08 23:29:38 +0200

    modify environment variable

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-du#eyo3u=2!de8=52wmre&n^rb1+^e&6u5=+ekh-kd$o@zlxbj'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
